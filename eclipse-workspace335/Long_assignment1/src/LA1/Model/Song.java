78d442f4559c024cfd722457b563318dc3055ded
BBudiharjo <BudimanaBudiharjo@gmail.com>
2025-02-15 16:46:58 -0700

    added variables, constructors and getters for Song

--- eclipse-workspace335/Long_assignment1/src/LA1/Model/Song.java
+++ eclipse-workspace335/Long_assignment1/src/LA1/Model/Song.java
@@ -1,10 +1,27 @@
 package LA1.Model;
 
 public class Song {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+	private String name;
+	private String artist;
+	private String album;
+	private int userRating;
+	
+	public Song(String song, String artist, String album){
+		this.name = song;
+		this.artist = artist;
+		this.album = album;
+	}
+	
+	public String getSongTitle(){
+		return this.name;
+	}
+	
+	public String getArtistName(){
+		return this.artist;
 	}
 
+	public String getAlbumTitle(){
+		return this.album;
+	}
+	
 }
