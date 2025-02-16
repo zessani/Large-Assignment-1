96bfad883e9301618b67b9fcf8f7c6a5a440f75e
zessani <zayyan.essani@gmail.com>
2025-02-15 17:24:44 -0700

    update album file

--- eclipse-workspace335/Long_assignment1/src/LA1/Model/Album.java
+++ eclipse-workspace335/Long_assignment1/src/LA1/Model/Album.java
@@ -1,12 +1,56 @@
 package LA1.Model;
 
+import java.util.ArrayList;
+
 public class Album {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String title;
+	private String artist;
+	private String genre;
+	private int year;
+	private ArrayList<Song> songs;
+	
+	
+	public Album (String title, String artist, String genre, int year){
+		this.title = title;
+		this.artist = artist;
+		this.genre = genre;
+		this.year = year;
+		this.songs = new ArrayList<>();
 		
-		// working on this today
-
+		
 	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getArtist() {
+		return artist;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public ArrayList<Song> getSongs(){
+		return new ArrayList<>(songs);
+	}
+	
+	public Integer countSongs() {
+		return songs.size()
+;	}
+	
+	//public Song getSong(String title) {
+		//
+	//}
+	
+	//public void addSong (String songTitle) {
+		
+	//}
 
 }
