1b0b8feccb7d89b376b478680664e1320a783729
zessani <zayyan.essani@gmail.com>
2025-02-21 18:14:03 -0700

    updated song method with rating and favorite

--- eclipse-workspace335/Long_assignment1/src/LA1/Model/Song.java
+++ eclipse-workspace335/Long_assignment1/src/LA1/Model/Song.java
@@ -5,11 +5,13 @@ public class Song {
 	private String artist;
 	private String album;
 	private int userRating;
+	private boolean favorite;
 	
 	public Song(String song, String artist, String album){
 		this.name = song;
 		this.artist = artist;
 		this.album = album;
+		this.favorite = false;
 	}
 	
 	public String getSongTitle(){
@@ -23,8 +25,27 @@ public class Song {
 	public String getAlbumTitle(){
 		return this.album;
 	}
+	// sets the rating between 1 and 5, and if rating is 5 auto set to favorite
+	public void setRating(int rating) {
+		if (rating >= 1 && rating <= 5) {
+			this.userRating = rating;
+			if (rating == 5) {
+				this.favorite = true;
+			}
+		}
+	}
 	
 	public int getRating(){
 		return this.userRating;
 	}
+	
+	public boolean isFavorite() {
+		return this.favorite;
+	}
+	
+	public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+	
+	
 }
