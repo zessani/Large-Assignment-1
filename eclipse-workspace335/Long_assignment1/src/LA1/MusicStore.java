682124be41f0d5dd3bd24f0477c9c4bd0ddc8e31
BBudiharjo <BudimanaBudiharjo@gmail.com>
2025-02-21 17:54:46 -0700

    remove unsure file

--- eclipse-workspace335/Long_assignment1/src/LA1/MusicStore.java
+++ eclipse-workspace335/Long_assignment1/src/LA1/MusicStore.java
@@ -10,37 +10,3 @@ public class MusicStore {
 		System.out.println("hello");
 	}
 	
-	public MusicStore initializeMusicStore(MusicStore musicStore){
-		String folder = "";
-		File folder = new File(folder);
-		File files[] = folder.listFiles();
-		BufferedReader br = new BufferedReader();
-		String[] albumInfoSplit = new String[4];
-		String albumInfo, album, artist, genre, year;
-		
-		for (int i = 0; i < files.length; i++){
-			albumInfo = br.readLine();
-			albumInfoSplit = albumInfo.split(',');
-			album = albumInfoSplit[0];
-			artist = albumInfoSplit[1];
-			genre = albumInfoSplit[2];
-			year = albumInfoSplit[3];
-			Album newAlbum = new Album(album,artist,genre,year);
-			while (true){
-				albumInfo = br.readLine();
-				if (albumInfo == null){
-					musicStore.addAlbum(newAlbum);
-					break;
-				}
-				else{
-					Song newSong = new Song(album,artist,album);
-					newAlbum.addSong(newSong);
-					musicStore.addSong(newSong);
-				}
-				musicStore.addAlbum(newAlbum);
-			}
-		}
-		return musicStore;
-	}
-
-}
